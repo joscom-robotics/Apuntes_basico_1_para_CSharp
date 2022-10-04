8f5f3eea0839fee25651ba55976966e88423b14a
Jos <jos1230@gmail.com>
2022-10-03 20:01:54 -0500

    Primer actualización

--- Apuntes_basico_1/Program.cs
+++ Apuntes_basico_1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello World ejercicios básicos en Sharp!");
-Console.WriteLine("Esta sección abarca sentencia básicas de escritura en C#.");
+Console.WriteLine("Esta sección abarca sentencias básicas de escritura y lectura en C#.");
 
 
 #region Comentarios
@@ -25,7 +25,7 @@ bool boleano = false;
 string cadena = "Esta cadena.";
 #endregion
 
-///Nulables
+///Nulables.
 #region Nulables. Secc 4.1
 bool? nulable = null; ///Agregando un ? puede aceptar valor nulo pudiendo cambiar posteriormente.
 int? numero_2 = null; ///Puede ser nulo pero posteriormente recibe un valor.
@@ -63,9 +63,38 @@ Console.WriteLine("Suma: " + suma);
 int resta = consUno - consDos;
 Console.WriteLine("Resta: " + resta);
 
-int multip = consUno + consDos;
+int multip = consUno * consDos;
 Console.WriteLine("Multiplicación: " + multip);
+#endregion
+
+Console.WriteLine("\nOperadores relacionales básicos: ");
+#region Operadores relacionales básicos:
+Console.WriteLine("\nIngresa un número entero.");
+int num = Int32.Parse(Console.ReadLine());
+if(consUno == num)
+{
+    Console.WriteLine("El número es igual a la constante.");
+}
+if(consUno != num)
+{
+    Console.WriteLine("El número no es igual la constante.");
+}
+if(num < consUno)
+{
+    Console.WriteLine("El número es menor a la constante.");
+}
+
 
 #endregion
 
+///Operadores lógicos.
+Console.WriteLine("\nOpetradores lógicos: ");
+#region operadores lógicos:
+String Y = "&&";
+String O = "||";
+
+Console.WriteLine("El opetradore lógico para condicional AND es: "+ Y);
+Console.WriteLine("El opetradore lógico para condicional OR es: " + O);
+#endregion
 //Console.ReadKey();
+
